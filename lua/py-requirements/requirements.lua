9d9ffb146a5fb8c36924d768c8ec2d6799715c98
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-25 20:10:51 -0800

    Support additional dependency types as requirements

--- lua/py-requirements/requirements.lua
+++ lua/py-requirements/requirements.lua
@@ -14,16 +14,22 @@ local function remove_whitespace(line)
 end
 
 ---@param line string
+---@param operator string
 ---@return string|nil
 ---@return string|nil
-local function equal_dependency(line)
-    local name, version = line:match('^(.*)==(.*)$')
+local function dependency(line, operator)
+    local name, version = line:match('^(.*)' .. operator .. '(.*)$')
     return name, version
 end
 
 ---@enum DependencyKind
 local DependencyKind = {
     EQUAL = 1,
+    LESS = 2,
+    LESS_OR_EQUAL = 3,
+    GREATER_OR_EQUAL = 4,
+    GREATER = 5,
+    COMPATIBLE = 6,
 }
 
 ---@class PythonModule
@@ -39,23 +45,39 @@ local M = {}
 ---@param line string
 ---@return PythonModule|nil
 function M.parse_module(line_number, line)
+    -- Reference
+    -- https://pip.pypa.io/en/stable/reference/requirements-file-format/
+    -- https://pip.pypa.io/en/stable/reference/requirement-specifiers/
+
     line = uncomment(line)
     line = remove_whitespace(line)
     if line:len() == 0 then
         return nil
     end
 
-    local name, version = equal_dependency(line)
-    if name and version then
-        ---@type PythonModule
-        return {
-            line_number = line_number,
-            name = name,
-            kind = DependencyKind.EQUAL,
-            version = version,
-            versions = {},
-        }
+    local dependency_operator = {
+        ['=='] = DependencyKind.EQUAL,
+        ['<'] = DependencyKind.LESS,
+        ['<='] = DependencyKind.LESS_OR_EQUAL,
+        ['>='] = DependencyKind.GREATER_OR_EQUAL,
+        ['>'] = DependencyKind.GREATER,
+        ['~='] = DependencyKind.COMPATIBLE,
+    }
+
+    for operator, kind in pairs(dependency_operator) do
+        local name, version = dependency(line, operator)
+        if name and version then
+            ---@type PythonModule
+            return {
+                line_number = line_number,
+                name = name,
+                kind = kind,
+                version = version,
+                versions = {},
+            }
+        end
     end
+
     return nil
 end
 
@@ -63,9 +85,6 @@ end
 ---@return PythonModule[]
 function M.parse_modules(buf)
     local modules = {}
-
-    -- Reference: https://pip.pypa.io/en/stable/reference/requirements-file-format/
-    -- Supports ignoring comments and equality versions
     local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
     for i, line in ipairs(lines) do
         local module = M.parse_module(i - 1, line)
@@ -73,7 +92,6 @@ function M.parse_modules(buf)
             table.insert(modules, module)
         end
     end
-
     return modules
 end
 
