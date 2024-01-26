5b5fbd15053f7145a3d8e17bcb05516c6ba4abe7
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-01-25 17:27:22 -0800

    Add icons to diagnostics

--- lua/py-requirements/ui.lua
+++ lua/py-requirements/ui.lua
@@ -5,15 +5,18 @@ local NAMESPACE = vim.api.nvim_create_namespace('py-requirements.nvim')
 local function to_diagnostic(module)
     local severity = vim.diagnostic.severity.INFO
     local latest_version = module.versions[#module.versions]
-    if latest_version and latest_version ~= module.version then
-        severity = vim.diagnostic.severity.WARN
+    if latest_version then
+        if latest_version ~= module.version then
+            severity = vim.diagnostic.severity.WARN
+        end
+        latest_version = ' ' .. latest_version
     end
     ---@type Diagnostic
     return {
         lnum = module.line_number,
         col = 0,
         severity = severity,
-        message = latest_version or 'Loading...',
+        message = latest_version or ' Loading',
         source = 'py-requirements',
     }
 end
