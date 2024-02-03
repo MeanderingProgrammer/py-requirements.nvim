7be4315c4dbb095b35fe6f3e4563f5605ca3f582
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-02-02 22:18:54 -0800

    Add more checks to unit tests

--- tests/init_spec.lua
+++ tests/init_spec.lua
@@ -4,6 +4,23 @@ local util = require('plenary.async.util')
 local ui = require('py-requirements.ui')
 local eq = assert.are.same
 
+---@param name string
+---@param versions string[]
+local function set_response(curl, name, versions)
+    curl.get
+        .on_call_with(string.format('https://pypi.org/simple/%s/', name), {
+            headers = {
+                Accept = 'application/vnd.pypi.simple.v1+json',
+                ['User-Agent'] = 'py-requirements.nvim (https://github.com/MeanderingProgrammer/py-requirements.nvim)',
+            },
+            raw = { '--location' },
+        })
+        .returns({
+            status = 200,
+            body = vim.json.encode({ versions = versions }),
+        })
+end
+
 async_tests.describe('init', function()
     async_tests.before_each(function()
         require('py-requirements').setup({
@@ -13,20 +30,25 @@ async_tests.describe('init', function()
 
     async_tests.it('run auto command', function()
         local curl = mock(require('plenary.curl'), true)
-        curl.get.returns({
-            status = 200,
-            body = vim.json.encode({ versions = { '3.2.2' } }),
-        })
+        set_response(curl, 'argcomplete', { '3.2.2' })
+        set_response(curl, 'pandas', { '2.1.0', '2.2.0' })
+
         vim.cmd('e tests/requirements.txt')
         util.scheduler()
-        assert.stub(curl.get).was_called_with('https://pypi.org/simple/argcomplete/', {
-            headers = {
-                Accept = 'application/vnd.pypi.simple.v1+json',
-                ['User-Agent'] = 'py-requirements.nvim (https://github.com/MeanderingProgrammer/py-requirements.nvim)',
-            },
-            raw = { '--location' },
-        })
-        local marks = vim.api.nvim_buf_get_extmarks(0, ui.TEXT_NAMESPACE, 0, -1, {})
-        eq(1, #marks)
+
+        local mark_details = {}
+        local marks = vim.api.nvim_buf_get_extmarks(0, ui.TEXT_NAMESPACE, 0, -1, { details = true })
+        for _, mark in ipairs(marks) do
+            local _, row, _, details = unpack(mark)
+            table.insert(mark_details, {
+                pos = { row, details.virt_text_win_col },
+                text = details.virt_text[1][1],
+            })
+        end
+        local expected = {
+            { pos = { 0, 23 }, text = ' 3.2.2' },
+            { pos = { 1, 23 }, text = ' 2.2.0' },
+        }
+        eq(expected, mark_details)
     end)
 end)
