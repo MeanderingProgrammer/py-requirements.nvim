fd005d449010919e4a9e05f2236efa47cfa4e2f2
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-05-31 08:50:49 -0700

    Use single query to capture module parameters rather than a query per parameter

--- lua/py-requirements/parser/requirements.lua
+++ lua/py-requirements/parser/requirements.lua
@@ -1,25 +1,43 @@
-local ts = require('py-requirements.parser.ts')
+---@class py.requirements.Node
+---@field value string
+---@field start_col integer
+---@field end_col integer
 
 ---@param source (integer|string)
 ---@param root TSNode
 ---@return py.requirements.ParsedPythonModule?
 local function parse_module(source, root)
-    local requirements = ts:new('requirements', source, root)
-    local name_node = requirements:query('(requirement (package) @package)')
-    if name_node == nil then
-        return nil
+    ---@type table<string,py.requirements.Node>
+    local captures = {}
+    local query = vim.treesitter.query.parse(
+        'requirements',
+        [[
+            (requirement (package) @name)
+            (version_spec (version_cmp) @cmp)
+            (version_spec (version) @version)
+        ]]
+    )
+    for id, node in query:iter_captures(root, source) do
+        local capture = query.captures[id]
+        local _, start_col, _, end_col = node:range()
+        ---@type py.requirements.Node
+        local py_node = {
+            value = vim.treesitter.get_node_text(node, source),
+            start_col = start_col,
+            end_col = end_col,
+        }
+        captures[capture] = py_node
     end
-    local comparison_node = requirements:query('(version_spec (version_cmp) @cmp)')
-    local comparison = nil
-    if comparison_node then
-        comparison = comparison_node.value
+
+    if captures.name == nil then
+        return nil
     end
     ---@type py.requirements.ParsedPythonModule
     return {
         line_number = root:start(),
-        name = name_node.value,
-        comparison = comparison,
-        version = requirements:query('(version_spec (version) @version)'),
+        name = captures.name.value,
+        comparison = vim.tbl_get(captures, 'cmp', 'value'),
+        version = captures.version,
     }
 end
 
@@ -31,7 +49,7 @@ function M.parse_modules(buf)
     local modules = {}
     local tree = vim.treesitter.get_parser(buf, 'requirements')
     local query = vim.treesitter.query.parse('requirements', '((requirement) @requirement)')
-    for _, node in query:iter_captures(tree:parse()[1]:root(), buf, 0, -1) do
+    for _, node in query:iter_captures(tree:parse()[1]:root(), buf) do
         local module = parse_module(buf, node)
         if module then
             table.insert(modules, module)
